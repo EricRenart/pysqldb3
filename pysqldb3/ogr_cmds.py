17b1abb44d9ec096b346385f32df838fc86de1ca
Eric Renart <blacksun9487@gmail.com>
2022-08-23 11:45:51 -0400

    qualified methods in ogr_cmds.py

--- pysqldb3/ogr_cmds.py
+++ pysqldb3/ogr_cmds.py
@@ -12,6 +12,7 @@ This file contains functions for performing queries with GDAL and OGR.
 Shapefile OGR Commands
 """
 def read_shapefile_pg(shp_path=None, tbl_name=None, schema='public', srid=2263, output_result=False):
+    # type: (str, str, str, int, bool) -> Optional(str)
     """
     Read in an ESRI shapefile and store its data in the Postgre database.
     :param shp_path: Path to the shapefile to be read
@@ -23,6 +24,7 @@ def read_shapefile_pg(shp_path=None, tbl_name=None, schema='public', srid=2263, 
     pass
 
 def read_shapefile_ms(shp_path=None, tbl_name=None, schema='public', srid=2263, output_result=False):
+    # type: (str, str, str, int, bool) -> Optional(str)
     """
     Read in an ESRI shapefile and store its data in the MSSQL database.
     :param shp_path: Path to the shapefile to be read
@@ -34,6 +36,7 @@ def read_shapefile_ms(shp_path=None, tbl_name=None, schema='public', srid=2263, 
     pass
 
 def write_shapefile_pg(shp_name=None, tbl_name=None, export_path=None, srid=2263, output_result=False):
+    # type: (str, str, str, int, bool) -> Optional(str)
     """
     Write out an ESRI Shapefile using data from the PostgreSQL database.
     :param shp_name: Filename of the shapefile to be created.
@@ -46,6 +49,7 @@ def write_shapefile_pg(shp_name=None, tbl_name=None, export_path=None, srid=2263
     pass
 
 def write_shapefile_ms(shp_name=None, tbl_name=None, export_path=None, srid=2263, output_result=False):
+    # type: (str, str, str, int, bool) -> Optional(str)
     """
     Write out an ESRI Shapefile using data from the MSSQL database.
     :param shp_name: Filename of the shapefile to be created.
@@ -62,6 +66,7 @@ Feature Class OGR Commands
 """
 
 def read_feature_class_pg(fc_name=None, geodatabase=None, tbl_name=None, schema='public', srid=2263, output_result=False):
+    # type: (str, str, str, str, int, bool) -> Optional(str)
     """
     Read in a Feature Class to the Postgre database.
     :param fc_name: Name of the feature class to read in
@@ -69,10 +74,12 @@ def read_feature_class_pg(fc_name=None, geodatabase=None, tbl_name=None, schema=
     :param tbl_name: The name of the table to store feature class data in.
     :param schema: The db schema to use. Default 'public'
     :param srid: Spatial reference identifier for coordinate system to use (default 2263 NAD83/NYLI-ft)
+    :param output_result: Whether to print out the results of the SQL query (default False)
     """
     pass
 
 def read_feature_class_ms(fc_name=None, geodatabase=None, tbl_name=None, schema='public', srid=2263, output_result=False):
+    # type: (str, str, str, str, int, bool) -> Optional(str)
     """
     Read in a Feature Class to the MSSQL database.
     :param fc_name: Name of the feature class to read in
@@ -80,6 +87,7 @@ def read_feature_class_ms(fc_name=None, geodatabase=None, tbl_name=None, schema=
     :param tbl_name: The name of the table to store feature class data in.
     :param schema: The db schema to use. Default 'public'
     :param srid: Spatial reference identifier for coordinate system to use (default 2263 NAD83/NYLI-ft)
+    :param output_result: Whether to print out the results of the SQL query (default False)
     """
     pass
 
@@ -87,6 +95,7 @@ def read_feature_class_ms(fc_name=None, geodatabase=None, tbl_name=None, schema=
 DB-DB IO Commands
 """
 def pgsql_to_mssql(source_table=None, dest_table=None, source_schema='public', dest_schema='public', query=False, use_ldap=False, output_result=False):
+    # type: (str, str, str, str, bool, bool, bool) -> Optional(str)
     """
     Export a table from a PostgreSQL database to a MSSQL Server database.
     :param source_table: The name of the source PostgreSQL table to convert to MSSQL.
@@ -95,10 +104,12 @@ def pgsql_to_mssql(source_table=None, dest_table=None, source_schema='public', d
     :param ms_schema: Database schema to use for destination MSSQL database (default 'public')
     :param query: Whether to run this command as an SQL Query (default False)
     :param use_ldap: Whether to use Lightweight Directory Access Protocol (LDAP) for db connection. (default False)
+    :param output_result: Whether to print out the results of the SQL query (default False)
     """
     pass
 
 def mssql_to_pgsql(source_table=None, dest_table=None, source_schema='public', dest_schema='public', query=False, use_ldap=False, output_result=False):
+    # type: (str, str, str, str, bool, bool, bool) -> Optional(str)
     """
     Export a table from a MS SQL Server database to a PostgreSQL database.
     :param source_table: The name of the source MSSQL table to convert to PostgreSQL.
@@ -107,9 +118,11 @@ def mssql_to_pgsql(source_table=None, dest_table=None, source_schema='public', d
     :param ms_schema: Database schema to use for source MSSQL database (default 'public')
     :param query: Whether to run this command as an SQL Query (default False)
     :param use_ldap: Whether to use Lightweight Directory Access Protocol (LDAP) for db connection. (default False)
+    :param output_result: Whether to print out the results of the SQL query (default False)
     """
 
 def pgsql_to_pgsql(source_table=None, dest_table=None, source_schema='public', dest_schema='public', query=False, use_ldap=False, output_result=False):
+    # type: (str, str, str, str, bool, bool, bool) -> Optional(str)
     """
     Export a table from a PostgreSQL database to another PostgreSQL database.
     :param source_table: The name of the source PostgreSQL table to export.
@@ -118,6 +131,7 @@ def pgsql_to_pgsql(source_table=None, dest_table=None, source_schema='public', d
     :param dest_schema: Schema of the destination table (default 'public')
     :param query: Whether to run this command as an SQL Query (default False)
     :param use_ldap: Whether to use Lightweight Directory Access Protocol (LDAP) for db connection. (default False)
+    :param output_result: Whether to print out the results of the SQL query (default False)
     """
     pass
 
@@ -131,6 +145,7 @@ def pg_bulk_file_to_table(paths, dest_table, schema='public', output_result=Fals
     :param paths: List of filepaths to import into table (corresponds to )
     :param dest_table: Name of table to be created in destination database
     :param schema: Database schema to use for import, default 'public'
+    :param output_result: Whether to print out the results of the SQL query (default False)
     """
     pass
 
@@ -141,5 +156,6 @@ def ms_bulk_file_to_table(paths, dest_table, schema='public', output_result=Fals
     :param paths: List of filepaths to import into table (corresponds to )
     :param dest_table: Name of table to be created in destination database
     :param schema: Database schema to use for import, default 'public'
+    :param output_result: Whether to print out the results of the SQL query (default False)
     """
     pass
